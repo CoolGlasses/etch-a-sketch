f1f12252a134ae4ed9060e2ce35036417aab4c99
Robin Andrea <robinandrea_2@yahoo.com>
2019-10-31 12:16:54 -0700

    Renamed CSS Grid attempt at solution to primary files.  Moved Flexbox based attempt to subdirectory. Added background color of navy and mouseover color change to gold

---
+++ flexbox-attempt/javascript.js
@@ -0,0 +1,47 @@
+let gridColumns = 16;
+let gridRows = 16;
+
+function createGridContainer() {
+    let gridContainer = document.createElement("div");
+    gridContainer.setAttribute("id", "gridContainer");
+    document.getElementById("mainBox").appendChild(gridContainer);
+}
+
+createGridContainer();
+
+
+function createBoard(gridRows, gridColumns) {
+    let z = 100000000000 + gridRows;  
+    for (let k = 0; k < gridRows; k++) {
+        let columnContainer = document.createElement("column");
+        columnContainer.setAttribute("class", "column");
+        columnContainer.setAttribute("id", k);
+        document.getElementById("gridContainer").appendChild(columnContainer);
+
+        for (let i = 0; i < gridColumns; i++) {
+            var newDiv = document.createElement("row")
+            newDiv.textContent = "i";
+            newDiv.setAttribute("id", z);
+            newDiv.setAttribute("class", "row");
+            newDiv.setAttribute("name", z);
+            document.getElementById(k).appendChild(newDiv);
+            document.getElementById(z).addEventListener("mouseover", function(){
+                this.style.backgroundColor = "black";
+            });
+            ++z;
+        }
+    }
+
+
+}
+
+
+createBoard(gridRows, gridColumns);
+
+document.getElementById("resetButton").addEventListener("click", function(){
+    let pleaseRemove = document.getElementById("gridContainer");
+    pleaseRemove.remove();
+    createGridContainer();
+    let boardSize = prompt("How many squares per side?");
+    createBoard(boardSize, boardSize);
+});
--- javascript.js
+++ javascript.js
@@ -2,43 +2,41 @@ let gridColumns = 16;
 let gridRows = 16;
 
 function createGridContainer() {
-    let gridContainer = document.createElement("div");
+    let gridContainer = document.createElement("gridContainer");
     gridContainer.setAttribute("id", "gridContainer");
+    gridContainer.setAttribute("class", "gridWrapper");
     document.getElementById("mainBox").appendChild(gridContainer);
 }
 
 createGridContainer();
 
 
+
 function createBoard(gridRows, gridColumns) {
-    let z = 100000000000 + gridRows;  
-    for (let k = 0; k < gridRows; k++) {
-        let columnContainer = document.createElement("column");
-        columnContainer.setAttribute("class", "column");
-        columnContainer.setAttribute("id", k);
-        document.getElementById("gridContainer").appendChild(columnContainer);
+        const gridContainer = document.querySelector("gridContainer");
+        gridContainer.style.setProperty("--colNum", gridColumns);
+        gridContainer.style.setProperty("--rowNum", gridRows);
+        let numberOfGridItems = gridRows * gridColumns;
+        createGridItems(numberOfGridItems);
+}
 
-        for (let i = 0; i < gridColumns; i++) {
-            var newDiv = document.createElement("row")
-            newDiv.textContent = "i";
-            newDiv.setAttribute("id", z);
-            newDiv.setAttribute("class", "row");
-            newDiv.setAttribute("name", z);
-            document.getElementById(k).appendChild(newDiv);
-            document.getElementById(z).addEventListener("mouseover", function(){
-                this.style.backgroundColor = "black";
-            });
-            ++z;
-        }
+function createGridItems(numberOfGridItems) {
+
+    for (let k = 0; k < numberOfGridItems; k++) {
+        let gridItem = document.createElement("gridItem");
+        gridItem.setAttribute("class", "gridItem");
+        gridItem.setAttribute("id", k);
+        document.getElementById("gridContainer").appendChild(gridItem);
+        document.getElementById(k).addEventListener("mouseover", function () {
+            this.style.backgroundColor = "gold";
+        });
     }
-
 
 }
 
-
 createBoard(gridRows, gridColumns);
 
-document.getElementById("resetButton").addEventListener("click", function(){
+document.getElementById("resetButton").addEventListener("click", function () {
     let pleaseRemove = document.getElementById("gridContainer");
     pleaseRemove.remove();
     createGridContainer();
